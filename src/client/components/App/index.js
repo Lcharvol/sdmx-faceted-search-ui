b409246fce850d965bc3026b8b7a2a5b78ca66ad
Mehdi AISSA-BRAHIM <maissa-b@e3r7p2.42.fr>
2017-07-24 20:34:48 +0200

    First push, app without css

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import Media from 'react-media';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
@@ -94,3 +95,12 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+=======
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from '../../../registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+>>>>>>> First push, app without css
