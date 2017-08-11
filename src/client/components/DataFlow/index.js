b374de4a143b96ab686375e20c444f7aeee57927
lcharvol <lcharvol@student.42.fr>
2017-08-11 10:55:08 +0200

    update

--- src/client/components/DataFlow/index.js
+++ src/client/components/DataFlow/index.js
@@ -5,12 +5,51 @@ import { Popover, Position, PopoverInteractionKind } from '@blueprintjs/core';
 
 import './DataFlow.css';
 
+const PopHoverElem = ({ data }) => (
+  <div className="popover_container ">
+    <div className="pophover_title_container">
+      <div className="pt-icon-info-sign logo_info pt-intent-primary" />
+      <p className="pophover_title">{data.name}</p>
+    </div>
+    <div className="text_container">
+      <p className="title">Id: </p>
+      <p>{data.id}</p>
+    </div>
+    <div className="text_container">
+      <p className="title">Agency: </p>
+      <p>{data.agency}</p>
+    </div>
+    <div className="text_container">
+      <p className="title">Version: </p>
+      <p>{data.version}</p>
+    </div>
+    {data.frequency && <div className="text_container">
+      <p className="title">Frequency: </p>
+      {data.frequency && data.frequency.map(frequence => (
+        <p>{frequence}</p>
+      ))}
+    </div>}
+    {data.interest_rate_type && <div className="text_container">
+      <p className="title">Interest rate type: </p>
+      {data.interest_rate_type && data.interest_rate_type.map(type => (
+        <p>{type}</p>
+      ))}
+    </div>}
+    {data.reference_area && <div className="text_container">
+      <p className="title">Reference area: </p>
+      <div className="ref_area_container">
+        {data.reference_area && data.reference_area.map(ref => (
+          <p>{ref}</p>
+        ))}
+      </div>
+    </div>}
+  </div>
+);
+
 const DataFlow = ({ data }) => (
   <Popover
     content={
-      <div className="popover_container">
-        <p>coucou</p>
-      </div>
+      <PopHoverElem data={data} />
     }
     target={
       <div className="pt-card pt-elevation-0 pt-interactive dataflowcontainer">
@@ -21,10 +60,15 @@ const DataFlow = ({ data }) => (
     position={Position.BOTTOM}
     interactionKind={PopoverInteractionKind.HOVER}
     openOnTargetFocus
+    inheritDarkTheme
   />
 );
 
 DataFlow.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+PopHoverElem.propTypes = {
   data: PropTypes.object.isRequired,
 };
 
